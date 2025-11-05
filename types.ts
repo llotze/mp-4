e819c243285c50ef74bf28149146500184ec266b
llotze <llotze@bu.edu>
2025-11-04 19:15:19 -0500

    added artwork type since unknown causes building errors

--- types.ts
+++ types.ts
@@ -8,8 +8,28 @@ export type ApiInfo = {
   responsetime: string;
 }
 
+export type Artwork = {
+  id?: number;
+  title?: string;
+  dated?: string;
+  classification?: string;
+  primaryimageurl?: string;
+  medium?: string;
+  culture?: string;
+  period?: string;
+  technique?: string;
+  dimensions?: string;
+  creditline?: string;
+  accessionyear?: number;
+  century?: string;
+  people?: Array<{
+    displayname: string;
+    role: string;
+  }>;
+}
+
 export type ApiResponse = {
-    info: ApiInfo;
-    records: unknown[];
-    aggregations: Record<string, unknown>;
+  info: ApiInfo;
+  records: Artwork[];
+  aggregations: Record<string, unknown>;
 }
